220fce1af8331bef551b0a973fc7c696abf7411e
Aviv Shbaty <shabtay.aviv@gmail.com>
2020-12-13 00:51:43 +0200

    Add Google mpas fragment
    * feat: Refacotoring map fragment
    * feat: Add logic to get user current location
    * feat: Add logic to update the map
    * feat: Add fragment to Last-Activity
    * feat: Rename fragment

---
+++ app/src/main/java/com/example/project_1/MapFragment.java
@@ -0,0 +1,195 @@
+package com.example.project_1;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback, LocationListener, ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private static final int PERMISSIONS_REQUEST_LOCATION = 99;
+    private final long LOCATION_REFRESH_TIME = 1; // 1 Minutes
+    private final float LOCATION_REFRESH_DISTANCE = 1; // 10 Meters
+
+    private View view;
+    private MapView mapView;
+    private GoogleMap map;
+    private LocationManager locationManager;
+    private LatLng position;
+    private Marker currentMarker;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Request user permissions:
+        if (checkLocationPermission()){
+            // Set the location manager to get user location:
+            locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+                    LOCATION_REFRESH_DISTANCE, this);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Bind the fragment class to the fragment layout:
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        this.mapView = (MapView) view.findViewById(R.id.top_MAP);
+        this.mapView.onCreate(savedInstanceState);
+        this.mapView.onResume();
+
+        // Register callback when the map ready:
+        this.mapView.getMapAsync(this);
+
+        this.view = view;
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.map = googleMap;
+        this.updateMap(this.position);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        this.mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        this.mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        this.mapView.onLowMemory();
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        this.position = new LatLng(lat, lng);
+        this.updateMap(this.position);
+    }
+
+    private void updateMap(LatLng newPosition){
+        if(this.position != null) {
+            // Remove existing marker if there is:
+            if (this.currentMarker != null) {
+                this.currentMarker.remove();
+            }
+
+            // Add new marker to the map:
+            this.currentMarker = this.map
+                    .addMarker(new MarkerOptions()
+                            .position(this.position));
+            this.map.moveCamera(CameraUpdateFactory.newLatLng(this.position));
+        }
+    }
+
+    /*  */
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(getContext())
+                        .setTitle("Request user location")
+                        .setMessage("Approve location services")
+                        .setPositiveButton(0, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(getActivity(),
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(getContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        //Request location updates:
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 400, 1, this);
+                    }
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return;
+            }
+
+        }
+    }
+}
+
+
